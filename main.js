ebc09f694ef242bbeccd5561d8c45499c032a9b5
Niklas Johansson <raphexion@gmail.com>
2016-04-09 14:14:10 +0200

    Adds more style and a red moving dot

---
+++ main.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    var ticktime = 5000;
+
+    var box = document.querySelector('.circle');
+    box.left = true;
+
+    function toggle(event) {
+        if (box.left) {
+            box.classList.remove('left');
+            box.classList.add('right');
+            box.classList.add('intransition');
+        } else {
+            box.classList.add('left');
+            box.classList.remove('intransition');
+            box.classList.remove('right');
+        }
+
+        box.left = !box.left;
+        window.setTimeout(toggle, ticktime);
+    };
+
+    window.setTimeout(toggle, ticktime);
+};
